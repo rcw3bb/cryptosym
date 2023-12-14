e874f28c30613bc88011bb35e3dda38ca802bf8a
Ron Webb <ron@ronella.xyz>
2023-12-14 16:11:36 +1300

    Make the version information coming from a single source.

--- cryptosym-gui/src/main/java/xyz/ronella/crypto/symmetric/gui/function/FullVersion.java
+++ cryptosym-gui/src/main/java/xyz/ronella/crypto/symmetric/gui/function/FullVersion.java
@@ -1,20 +1,27 @@
 package xyz.ronella.crypto.symmetric.gui.function;
 
 import xyz.ronella.crypto.symmetric.gui.common.impl.Version;
+import xyz.ronella.trivial.decorator.StringBuilderAppender;
 
+import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.function.Supplier;
+
+import static java.util.function.Predicate.not;
 
 public class FullVersion implements Supplier<String> {
     @Override
     public String get() {
-        var version = new Version();
-        var fullVersion = new StringJoiner(".")
+        final var version = new Version();
+
+        final var buildVersion = Optional.of(version.getBuild()).filter(not(String::isBlank)).orElse("GA");
+
+        final var fullVersion = new StringJoiner(".")
                 .add(version.getMajor().toString())
                 .add(version.getMinor().toString())
                 .add(version.getMaintenance())
-                .add(version.getBuild())
                 .toString();
-        return fullVersion;
+
+        return fullVersion + "-" + buildVersion;
     }
 }
